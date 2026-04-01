2f1e9ece3d3b9f8c795a1c460585df14b50c8763
karelsima04 <simaka03@jcu.cz>
2026-04-01 23:54:21 +0000

    update

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+from settings import *
 
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Western Survivor')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+
+            # delta time
+            dt = self.clock.tick() / 1000
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # update
+
+            # draw
+            pygame.display.update()
+
+        pygame.quit()
+
+game = Game()
+game.run()
+            
+
+
+    
